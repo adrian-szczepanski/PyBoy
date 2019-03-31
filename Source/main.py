d332b379ca91bd0675a8cc5567315c7d0a2f7ccf
Mads Ynddal <mads-git@ynddal.dk>
2019-03-31 14:10:46 +0200

    Fixed main.py for Py3

--- Source/main.py
+++ Source/main.py
@@ -21,11 +21,11 @@ addConsoleHandler()
 
 def getROM(ROMdir):
     # Give a list of ROMs to start
-    found_files = filter(lambda f: f.lower().endswith(
-        ".gb") or f.lower().endswith(".gbc"), os.listdir(ROMdir))
+    found_files = list(filter(lambda f: f.lower().endswith(
+        ".gb") or f.lower().endswith(".gbc"), os.listdir(ROMdir)))
     for i, f in enumerate(found_files):
         print ("%s\t%s" % (i + 1, f))
-    filename = raw_input("Write the name or number of the ROM file:\n")
+    filename = input("Write the name or number of the ROM file:\n")
 
     try:
         filename = ROMdir + found_files[int(filename) - 1]
